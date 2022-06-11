cc7dcb4fc81f3ab4cb1be0c55082b2da1ed6e99f
Anakhit <105109782+Anakhit@users.noreply.github.com>
2022-06-11 11:02:00 +0400

    Add files via upload

---
+++ task.js
@@ -0,0 +1,56 @@
+
+
+//Task #1
+let name = "ანა";
+let age = "28";
+let name0 = "ლევან";
+let age0= "21";
+let ageDifference = age; - age0;
+
+let result = "ანა" + "ლევან"+"ზე" ageDifference, "წლით დიდია";
+
+console.log(result);
+
+
+//Task #2
+
+let studentsArr = ['John','Sam','Ann','Will','Andrew','Joseph','Lorelai','Amelie','Den'];
+let middleArr = (students.length - 1)/2
+
+console.log(middleArr)
+
+
+//Task #3
+let student = {
+    firstName = "Jane",
+    lastName = "Doe",
+    age = "28",
+    fullName = "Jane" + "Doe",
+    subjectsArr = ["science", "math", "macroeconomics", "sociology", "literature"],
+    roommate = {
+        fullname = "John Doe",
+        age = "29",
+    }
+}
+let result = fullName + " არის" + age; + " წლის და მისი რუმმეითი არის " fullname;
+for(let i=0, i<subjectsArr.length; i++){
+    console.log(subjectsArr)
+}
+
+//task #4
+
+let fruitArr = ["Banana", "Orange", "Apple", "Mango",2,12]
+while(i < 3){
+    console.log(fruitArr)
+}
+
+//task #5
+
+let listArr = [12,23,43,11,9,2,121,90]
+i=0
+if (listArr[i]>31 && listArr[i]%2==0){
+    console.log('Element is greater than provided value and is EVEN')
+}
+if (listArr[i]<31 && listArr[i]%2 >0){
+    console.log('Element is less than provided value and is ODD')
+}
